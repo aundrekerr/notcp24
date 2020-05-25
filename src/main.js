7841ec2c99b8957f87f4a98075ec3f259ef4304c
aundre_kerr <hello@aundrekerr.com>
2020-05-24 21:21:07 -0400

    Disabled offline listener

--- src/main.js
+++ src/main.js
@@ -18,14 +18,14 @@ new Vue({
 }).$mount('#app');
 
 
-window.addEventListener('load', () => {
-  function handleNetworkChange() {
-    if (navigator.onLine) {
-      document.body.classList.remove('offline');
-    } else {
-      document.body.classList.add('offline');
-    }
-  }
-  window.addEventListener('online', handleNetworkChange);
-  window.addEventListener('offline', handleNetworkChange);
-});
+// window.addEventListener('load', () => {
+//   function handleNetworkChange() {
+//     if (navigator.onLine) {
+//       document.body.classList.remove('offline');
+//     } else {
+//       document.body.classList.add('offline');
+//     }
+//   }
+//   window.addEventListener('online', handleNetworkChange);
+//   window.addEventListener('offline', handleNetworkChange);
+// });
